31a45f1c7907051c234f096a3e30199350e45f3f
Josh Wulf <josh.wulf@camunda.com>
2024-04-07 12:05:43 +1200

    test(repo): fix relative path in jest setup

--- src/__tests__/config/jest.cleanup.ts
+++ src/__tests__/config/jest.cleanup.ts
@@ -6,9 +6,10 @@ import path from 'path'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('tsconfig-paths').register()
 
-import { OAuthProvider } from 'oauth'
-import { OperateApiClient } from 'operate'
 import { BpmnParser, ZeebeGrpcClient } from 'zeebe'
+
+import { OAuthProvider } from '../../oauth'
+import { OperateApiClient } from '../../operate'
 
 export const cleanUp = async () => {
 	// Your cleanup process here.
