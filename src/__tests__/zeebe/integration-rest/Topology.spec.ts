c51055239dde419cde4421ce22870451fbbbedd9
Josh Wulf <josh.wulf@camunda.com>
2024-05-21 11:12:52 +1200

    test(zeebe): fix Topology Rest Client test

--- src/__tests__/zeebe/integration-rest/Topology.spec.ts
+++ src/__tests__/zeebe/integration-rest/Topology.spec.ts
@@ -1,7 +1,7 @@
-import { ZeebeRESTClient } from '../../../zeebe'
+import { ZeebeRestClient } from '../../../zeebe'
 
 test('it can get the topology', async () => {
-	const zbc = new ZeebeRESTClient()
+	const zbc = new ZeebeRestClient()
 	const topology = await zbc.getTopology()
 	expect(topology).toHaveProperty('brokers')
 })
