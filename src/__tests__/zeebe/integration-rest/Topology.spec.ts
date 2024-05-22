e7bcca68e4cd7ec06a41216cf39ae90af8c96977
Josh Wulf <josh.wulf@camunda.com>
2024-05-22 17:13:33 +1200

    test(zeebe): improve test structure

--- src/__tests__/zeebe/integration-rest/Topology.spec.ts
+++ src/__tests__/zeebe/integration-rest/Topology.spec.ts
@@ -1,7 +1,9 @@
 import { ZeebeRestClient } from '../../../zeebe'
 
-test('it can get the topology', async () => {
-	const zbc = new ZeebeRestClient()
-	const topology = await zbc.getTopology()
-	expect(topology).toHaveProperty('brokers')
+describe('ZeebeRestClient', () => {
+	it('can get the topology', async () => {
+		const zbc = new ZeebeRestClient()
+		const topology = await zbc.getTopology()
+		expect(topology).toHaveProperty('brokers')
+	})
 })
