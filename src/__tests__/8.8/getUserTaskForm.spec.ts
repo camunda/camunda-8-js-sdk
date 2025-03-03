81dfe5a33a7bac04fb8fcfbf92d565f00243a31b
Josh Wulf <josh.wulf@camunda.com>
2025-03-03 17:06:39 +1300

    test: move getUserTaskForm to 8.8 tests

--- src/__tests__/8.8/getUserTaskForm.spec.ts
+++ src/__tests__/8.8/getUserTaskForm.spec.ts
@@ -1,21 +1,41 @@
-import { CamundaRestClient } from '../../../c8/lib/CamundaRestClient'
+import { CamundaRestClient } from '../../c8/lib/CamundaRestClient'
+import { delay } from '../../lib'
 
 const c8 = new CamundaRestClient()
-
+jest.setTimeout(25000)
 test('It can retrieve a linked user task form', async () => {
 	const res = await c8.deployResourcesFromFiles([
 		'./src/__tests__/testdata/form/test-form-retrieval.bpmn',
+		'./src/__tests__/testdata/form/test-basic-form.form',
 	])
-	const key = res.processes[0].processDefinitionKey
-	const id = res.processes[0].processDefinitionId
+	const definitionKey = res.processes[0].processDefinitionKey
+	const definitionId = res.processes[0].processDefinitionId
 	const wfi = await c8.createProcessInstance({
-		processDefinitionId: id,
+		processDefinitionId: definitionId,
 		variables: {},
 	})
-	expect(wfi.processDefinitionKey).toBe(key)
+	const processInstanceKey = wfi.processInstanceKey
+
+	expect(wfi.processDefinitionKey).toBe(definitionKey)
 	// Search user tasks
-	// After deleting the process definition, we should not be able to start a new process instance.
-	await expect(
-		c8.createProcessInstance({ processDefinitionId: id, variables: {} })
-	).rejects.toThrow('404')
+
+	await delay(7000)
+	console.log({
+		filter: { processInstanceKey },
+		page: { from: 0, limit: 100 },
+		sort: [{ field: 'priority', order: 'asc' }],
+	})
+	const tasks = await c8.searchUserTasks({
+		filter: { processInstanceKey },
+		page: { from: 0, limit: 100 },
+		sort: [{ field: 'priority', order: 'asc' }],
+	})
+	// tslint:disable-next-line: no-console
+	console.log('tasks', tasks) // @DEBUG
+
+	expect(tasks.items.length).toBe(1)
+	// Now retrieve the form
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const form = await c8.getUserTaskForm((tasks.items[0] as any).key)
+	expect(form.formKey).toBe(tasks.items[0].formKey)
 })
