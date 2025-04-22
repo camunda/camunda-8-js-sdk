60bced64738ae59a4912070f64c77d03cc503c97
Josh Wulf <josh.wulf@camunda.com>
2025-04-22 19:22:54 +1200

    test: update callApiEndpoint tests

--- src/__tests__/c8/rest/callApiEndpoint.rest.spec.ts
+++ src/__tests__/c8/rest/callApiEndpoint.rest.spec.ts
@@ -9,29 +9,7 @@ beforeAll(async () => {
 		path.join('.', 'src', '__tests__', 'testdata', `test-drd.dmn`),
 	])
 })
-test('it can call 8.7 alpha search endpoint', async () => {
-	const res = await restClient.callApiEndpoint({
-		urlPath: 'process-instances/search',
-		method: 'POST',
-		body: {
-			filter: {
-				completed: true,
-			},
-			sort: [
-				{
-					field: 'bpmnProcessId',
-					order: 'desc',
-				},
-				{
-					field: 'processDefinitionKey',
-					order: 'asc',
-				},
-			],
-		},
-	})
-	console.log(res)
-	expect(res).toBeTruthy()
-
+test('it can call an endpoint', async () => {
 	const res2 = await restClient.callApiEndpoint({
 		urlPath: 'decision-definitions/evaluation',
 		method: 'POST',
