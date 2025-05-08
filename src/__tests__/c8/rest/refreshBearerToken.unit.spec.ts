c38fef74f3b5d54c63c4c364e4806e13e013037f
Josh Wulf <josh.wulf@camunda.com>
2025-05-08 14:18:48 +1200

    test: fix refreshBearerToken test

--- src/__tests__/c8/rest/refreshBearerToken.unit.spec.ts
+++ src/__tests__/c8/rest/refreshBearerToken.unit.spec.ts
@@ -4,40 +4,12 @@ import { Server, ServerCredentials, loadPackageDefinition } from '@grpc/grpc-js'
 import { loadSync } from '@grpc/proto-loader'
 
 import { Camunda8 } from '../../../c8'
-import { CamundaRestClient } from '../../../c8/lib/CamundaRestClient'
 import {
 	BrokerInfo,
 	Partition,
 	TopologyResponse,
 } from '../../../generated/zeebe_pb'
-import { Camunda8ClientConfiguration } from '../../../lib/Configuration'
-import { BearerAuthProvider, IOAuthProvider } from '../../../oauth'
-
-class TestCamundaRestClient extends CamundaRestClient {
-	constructor(options?: {
-		config?: Camunda8ClientConfiguration
-		oAuthProvider?: IOAuthProvider
-	}) {
-		super(options)
-	}
-	getBearerToken() {
-		return this.oAuthProvider.getToken('ZEEBE')
-	}
-}
-
-test('it can refresh the bearer token', async () => {
-	const client = new TestCamundaRestClient({
-		config: {
-			CAMUNDA_AUTH_STRATEGY: 'BEARER',
-			CAMUNDA_OAUTH_TOKEN: 'initialToken',
-		},
-	})
-	const initialToken = await client.getBearerToken()
-	expect(initialToken.authorization).toEqual('Bearer initialToken')
-	client.setBearerToken('newToken')
-	const newToken = await client.getBearerToken()
-	expect(newToken.authorization).toEqual('Bearer newToken')
-})
+import { BearerAuthProvider } from '../../../oauth'
 
 test('it can refresh the bearer token with a custom OAuth provider', async () => {
 	let currentToken = ''
